8ade92c0e495f1e2ea519a80f0c5ba1823b0202e
Piotr Henczel <henczel.piotr@gmail.com>
2018-09-24 22:41:56 +0200

    v 1.0

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var main = function() {
+    //display page when loaded
+    //displey page end
     //make random number
     $('#random-number').text(Math.floor((Math.random() * 1000) + 1));
 
@@ -31,53 +33,47 @@ var main = function() {
     //statistics animation on scroll
     let window_pos = $(window).scrollTop() + $(window).height();
 
-    let statistics_pos = $("#statistics").offset().top;
+    let statistics_pos_top = $("#statistics").offset().top;
+
+    let statistics_pos_bottom = $("#statistics").offset().bottom;
 
     let statistics_visible = false;
 
     let block_animation = false;
 
-    $(window).on("scroll", function() {
-        window_pos = $(window).scrollTop() + $(window).height();
-        if ((window_pos > statistics_pos)) {
-            statistics_visible = true;
-        }
-        if (statistics_visible && !block_animation) {
-            block_animation = true;
-            $('.statistics-number').each(function() {
-                $(this).prop('Counter', 0).animate({
-                    Counter: $(this).text()
-                }, {
-                    duration: 3000,
-                    easing: 'swing',
-                    step: function(now) {
-                        $(this).text(Math.ceil(now));
-                    }
+    $(window).on('load', function() {
+        $(window).on("scroll", function() {
+            window_pos = $(window).scrollTop() + $(window).height();
+            if ((window_pos > statistics_pos_top)) {
+                statistics_visible = true;
+            }
+            if (statistics_visible && !block_animation) {
+                block_animation = true;
+                $('.statistics-number').each(function() {
+                    $(this).prop('Counter', 0).animate({
+                        Counter: $(this).text()
+                    }, {
+                        duration: 3000,
+                        easing: 'swing',
+                        step: function(now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
                 });
-            });
 
-        }
+            }
+        });
     });
 
-    if (window_pos > statistics_pos) {
-        statistics_visible = true;
-    }
 
-    if (statistics_visible && !block_animation) {
-        block_animation = true;
-        $('.statistics-number').each(function() {
-            $(this).prop('Counter', 0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 3000,
-                easing: 'swing',
-                step: function(now) {
-                    $(this).text(Math.ceil(now));
-                }
-            });
-        });
-    }
+
     //statistic animation on scroll
+
+    //prevent default submit
+    $('#form-submit').click(function(e) {
+        e.preventDefault();
+    });
+    //prevent default submit end
 };
 
 $(document).ready(main);
